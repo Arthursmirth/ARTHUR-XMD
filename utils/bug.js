a5207f821836be40609067e11b97bfb044cc7915
Arthursmirth <thomasguy1975@gmail.com>
2026-03-08 23:18:27 -0400

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 ARTHUR TECH 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029Vb7z3g3L2AU1PcP5ea3d`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
