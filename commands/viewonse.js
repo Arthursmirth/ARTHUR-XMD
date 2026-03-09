bce2ae531e033db6370c7f929f8f36211bae43ea
Arthursmirth <thomasguy1975@gmail.com>
2026-03-09 14:15:44 -0500

    Update viewonce.js

--- commands/viewonse.js
+++ commands/viewonse.js
@@ -1,4 +1,4 @@
-import { DigixNew } from '../utils/DigixNew.js';
+iimport { DigixNew } from '../utils/DigixNew.js';
 import { downloadMediaMessage } from 'baileys';
 import fs from 'fs';
 import path from 'path';
